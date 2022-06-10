53fd91996bd33f3eebc4b32de2ec2f63ef584775
galsh26 <galsh26@gmail.com>
2022-06-10 20:28:46 +0300

    added register screen

--- app/src/main/java/com/example/advancedandroid/LoginActivity.java
+++ app/src/main/java/com/example/advancedandroid/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.advancedandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button registerButton = findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(v -> {
+            Intent i = new Intent(this, RegisterActivity.class);
+            startActivity(i);
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/advancedandroid/RegisterActivity.java
@@ -0,0 +1,55 @@
+package com.example.advancedandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        EditText password = findViewById(R.id.password_toggle1);
+        EditText validPassword = findViewById(R.id.passwordValidation);
+        final String pass = password.getText().toString();
+        final String validPass = validPassword.getText().toString();
+        int check = checkPassword(pass);
+
+        Button registerButton = findViewById(R.id.registerButton1);
+        registerButton.setOnClickListener(v -> {
+            boolean passwords;
+            passwords = (check == 1) && pass.equals(validPass);
+            // need to fix it and add check for empty fields
+            if (!passwords) {
+                TextView checkPasswords = findViewById(R.id.checkPasswords);
+                checkPasswords.setText("please fix the passwords");
+
+            } else {
+                // change this to the chats screen
+                Intent i = new Intent(this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    public int checkPassword(String password) {
+        if (password.isEmpty()) {
+            return 0;
+        }
+        // if the password is all numbers
+        if (password.matches("[0-9]+")) {
+            return 0;
+        }
+        // if the password has no numbers
+        if (password.matches("[a-zA-Z]+")) {
+            return 0;
+        }
+        return 1;
+    }
+}
