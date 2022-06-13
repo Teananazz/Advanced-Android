d6aee225a4d3e6f8f920d1e5911e299e1231c69d
Nitzan <nitzan893@gmail.com>
2022-06-13 20:15:09 +0300

    register and settings screen

---
+++ app/src/main/java/com/example/advancedandroid/ChangeUsernameActivity.java
@@ -0,0 +1,14 @@
+package com.example.advancedandroid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChangeUsernameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_username);
+    }
+}
