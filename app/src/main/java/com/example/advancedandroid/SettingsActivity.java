c6ce5b1a77f578233f254f0deb36eba89dd18364
Nitzan <nitzan893@gmail.com>
2022-06-19 15:01:46 +0300

    change server and theme

--- app/src/main/java/com/example/advancedandroid/SettingsActivity.java
+++ app/src/main/java/com/example/advancedandroid/SettingsActivity.java
@@ -2,10 +2,16 @@ package com.example.advancedandroid;
 
 import static com.example.advancedandroid.api.Api.BASE_URL;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.example.advancedandroid.api.Api;
 import com.example.advancedandroid.api.RetrofitClient;
@@ -23,9 +29,25 @@ public class SettingsActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
+        @SuppressLint("WrongViewCast") Switch s = findViewById(R.id.ChangeTheme);
+        s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (s.isChecked()) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    s.setText("Light Mode");
+                    reset();
+                }
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    s.setText("Dark Mode");
+                    reset();
+                }
+            }
+        });
         api = RetrofitClient.getInstance().getMyApi();
-        String str = "we will insert here the text from the screen";
+        EditText server = findViewById(R.id.NewServer);
+        String str = server.getText().toString();
         String httpStr = "https://";
         String apiStr = "/api/";
         if (!str.contains(httpStr)) {
@@ -39,5 +61,11 @@ public class SettingsActivity extends Activity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         api = retrofit.create(Api.class);
+    }
+
+    private void reset(){
+        Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
