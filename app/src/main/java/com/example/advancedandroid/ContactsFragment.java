8d51f4abd71bbcd51f2807592e497150ac57aa8a
Nitzan <nitzan893@gmail.com>
2022-06-19 17:37:46 +0300

    add some stuff
    add settings activity button

--- app/src/main/java/com/example/advancedandroid/ContactsFragment.java
+++ app/src/main/java/com/example/advancedandroid/ContactsFragment.java
@@ -1,10 +1,12 @@
 package com.example.advancedandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -45,6 +47,13 @@ public class ContactsFragment extends Fragment {
         // super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.contactsfragment, container, false);
         Activity = (AppActivity) getActivity();
+        Button b = view.findViewById(R.id.settingsButton);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(view.getContext(), SettingsActivity.class));
+            }
+        });
         if(Activity != null) {
 
             BearToken = Activity.Token_Bear;
